ecffef1515eeb4c0378cb44ff3165b7ac47a864d
Martin Pettersson <martin.pettersson@omegapoint.se>
2017-01-25 11:29:48 +0100

    just another plot

--- plots.py
+++ plots.py
@@ -398,9 +398,18 @@ def plot_lorenz2():
 	ax.set_ylabel('y')
 	ax.set_zlabel('z')
 	plt.show()
-	
-	
-	
+
+def another_plot():
+	fig=plt.figure()
+	ax=fig.gca(projection='3d')
+	x=np.arange(-5,5,0.25)
+	y=np.arange(-5,5,0.25)
+	x,y=np.meshgrid(x,y)
+	r=np.sqrt(x**2+y**2)
+	z=np.sin(r)
+	surf=ax.plot_surface(x,y,z,rstride=1,cstride=1,cmap=cm.coolwarm)
+	plt.show()
+		
 #mandelbrot_main()
 #step_lorenz()
 #simple_animation()
@@ -421,5 +430,5 @@ def plot_lorenz2():
 #trisurf()
 #hillshading()
 #plot_lorenz2()
-
+another_plot()
 
