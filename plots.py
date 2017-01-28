6603e4947040392c189e8cb19f55c78f73f3ecca
Martin Pettersson <martin.pettersson@omegapoint.se>
2017-01-28 21:22:42 +0100

    Basic animation

--- plots.py
+++ plots.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import matplotlib.cm as cm
+import matplotlib.animation as animation
 import time
 
 def lorenz(x, y, z, s=10, r=28, b=2.667):
@@ -457,7 +458,40 @@ def another_graphic():
 	ax.plot_surface(x,y,z,rstride=1,cstride=1,cmap='hot')
 	plt.show()
 	
-another_graphic()		
+def data_gen(t=0):
+	cnt=0
+	while cnt<1000:
+		cnt+=1
+		t+=0.1
+		yield t,np.sin(2*np.pi*t)*np.exp(-1/10)
+
+def init():
+	ax.set_ylim(-1.1,1.1)
+	ax.set_xlim(0,10)
+	del xdata[:]
+	del ydata[:]
+	line.set_data(xdata,ydata)
+	return line,
+
+fig,ax=plt.subplots()
+line,=ax.plot([],[],lw=2)
+ax.grid()
+xdata,ydata=[],[]
+
+def run(data):
+	t,y=data
+	xdata.append(t)
+	ydata.append(y)
+	xmin,xmax=ax.get_xlim()
+	if t>=xmax:
+		ax.set_xlim(xmin,2*xmax)
+		ax.figure.canvas.draw()
+	line.set_data(xdata,ydata)
+	return line,
+ani=animation.FuncAnimation(fig,run,data_gen,blit=False,interval=10,repeat=False,init_func=init)
+plt.show()
+
+#another_graphic()		
 #mandelbrot_main()
 #step_lorenz()
 #simple_animation()
