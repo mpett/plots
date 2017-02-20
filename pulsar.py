c9bf0ebf590609dff323163a7cef5c599c3e6968
Martin Pettersson <kontakt@martinpettersson.se>
2017-02-20 13:30:46 +0100

    contour plot

--- pulsar.py
+++ pulsar.py
@@ -97,6 +97,18 @@ def wireframe():
 	fig=p.figure()
 	ax=p3.Axes3D(fig)
 	ax.plot_surface(x,y,z)
+	p.show()
+	delta=0.025
+	x=arange(-3.0,3.0,delta)
+	y=arange(-2.0,2.0,delta)
+	X,Y=p.meshgrid(x,y)
+	Z1=p.bivariate_normal(X,Y,1.0,1.0,0.0,0.0)
+	Z2=p.bivariate_normal(X,Y,1.5,0.5,1,1)
+	Z=10.0 * (Z2-Z1)
+	fig=p.figure()
+	ax=p3.Axes3D(fig)
+	ax.contourf3D(X,Y,Z)
+	fig.add_axes(ax)
 	p.show()	
 
 def mandelbrot():
