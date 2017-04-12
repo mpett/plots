edebee4ac0e1adbb2b6e99085c923da670c13dbf
Martin Pettersson <martin.pettersson@omegapoint.se>
2017-04-12 17:07:17 +0200

    Refactored basic tf multiplication example.

--- tfbasics.py
+++ tfbasics.py
@@ -4,6 +4,13 @@ a=tf.placeholder("float")
 b=tf.placeholder("float")
 y=tf.multiply(a,b)
 
-with tf.Session() as sess:
-	print("%f should equal 2.0" % sess.run(y,feed_dict={a: 1,b:2}))
-	print("%f should equal 9.0" % sess.run(y,feed_dict={a:3,b:3}))
+def multiplication_basics():
+	with tf.Session() as sess:
+		print("%f should equal 2.0" % sess.run(y,feed_dict={a: 1,b:2}))
+		print("%f should equal 9.0" % sess.run(y,feed_dict={a:3,b:3}))
+
+
+def main():
+	multiplication_basics()
+
+main()
