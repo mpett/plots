1a9694c6130594a6983b2cf2621dfaa4e3cb9599
Martin Pettersson <martin.pettersson@omegapoint.se>
2017-04-12 17:36:36 +0200

    Tf matrix multiplication

--- tfbasics.py
+++ tfbasics.py
@@ -15,9 +15,17 @@ def hello_world():
 	sess=tf.Session()
 	print(sess.run(hello))
 
+def matrix_multiplication():
+	matrix1=tf.constant([[3.,3.]])
+	matrix2=tf.constant([[2.],[2.]])
+	product=tf.matmul(matrix1,matrix2)
+	with tf.Session() as sess:
+		result=sess.run(product)
+		print(result)
 
 def main():
-	multiplication_basics()
-	hello_world()
+	#multiplication_basics()
+	#hello_world()
+	matrix_multiplication()
 
 main()
