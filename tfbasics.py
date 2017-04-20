5cc51c714e327764d27dd1c850d08df996ca010c
Martin Pettersson <martin.pettersson@omegapoint.se>
2017-04-20 14:44:59 +0200

    Tf, nearest neighbor

--- tfbasics.py
+++ tfbasics.py
@@ -23,9 +23,33 @@ def matrix_multiplication():
 		result=sess.run(product)
 		print(result)
 
+def nearest_neighbor():
+	import numpy as np
+	from tensorflow.examples.tutorials.mnist import input_data
+	mnist=input_data.read_data_sets("tmp/data",one_hot=True)
+	Xtr,Ytr=mnist.train.next_batch(5000)
+	Xte,Yte=mnist.test.next_batch(200)
+	xtr=tf.placeholder("float", [None,784])
+	xte=tf.placeholder("float",[784])
+	distance=tf.reduce_sum(tf.abs(tf.add(xtr,tf.negative(xte))),reduction_indices=1)
+	pred=tf.arg_min(distance,0)
+	accuracy=0.
+	init=tf.global_variables_initializer()	
+	with tf.Session() as sess:
+		sess.run(init)
+		for i in range(len(Xte)):
+			nn_index=sess.run(pred,feed_dict={xtr:Xtr,xte:Xte[i,:]})
+			print("Test",i,"Prediction:",np.argmax(Ytr[nn_index]), \
+				"True Class:",np.argmax(Yte[i]))
+			if np.argmax(Ytr[nn_index])==np.argmax(Yte[i]):
+				accuracy+=1./len(Xte)
+		print("Done!")
+		print("Accuracy:",accuracy)	
+
 def main():
 	#multiplication_basics()
 	#hello_world()
-	matrix_multiplication()
+	#matrix_multiplication()
+	nearest_neighbor()
 
 main()
